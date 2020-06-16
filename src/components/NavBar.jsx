df9c4bdcc9da0f478b7c5a777d915e80162cfd00
Detart <63840243+gdetart@users.noreply.github.com>
2020-06-16 13:06:14 +0200

    Added Search Functionality !

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,39 +1,127 @@
-import React, { Component } from 'react'
-import {Navbar, Nav , FormControl , Form,Container,Row} from 'react-bootstrap'
-import { GrLinkedin, } from 'react-icons/gr';
-import {AiOutlineHome,AiOutlineTeam , AiOutlinePlaySquare} from 'react-icons/ai'
-import {RiBriefcaseLine} from 'react-icons/ri'
-import {MdMessage} from 'react-icons/md'
-import {IoMdNotificationsOutline} from 'react-icons/io'
-import photo from '../images/photo.png'
-import {BsGrid3X3GapFill} from 'react-icons/bs'
+import React, { Component } from "react";
+import {
+  Navbar,
+  Nav,
+  FormControl,
+  Form,
+  Container,
+  Row,
+  Dropdown
+} from "react-bootstrap";
+import { GrLinkedin } from "react-icons/gr";
+import {
+  AiOutlineHome,
+  AiOutlineTeam,
+  AiOutlinePlaySquare,
+} from "react-icons/ai";
+import { RiBriefcaseLine } from "react-icons/ri";
+import { MdMessage } from "react-icons/md";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import photo from "../images/photo.png";
+import { BsGrid3X3GapFill } from "react-icons/bs";
+import {Link} from 'react-router-dom'
 export default class NavBar extends Component {
-    
-     
-    render() {
-        return (
-        <Navbar  className="navbar mt-0 fixed-top " style={{height: "55px"}} variant="dark">
-          <Container>              
-              <Navbar.Brand href="#home"  ><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Linkedin_icon.svg/1200px-Linkedin_icon.svg.png" className="linked"></img> </Navbar.Brand>
-              <Form >
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" style={{width:"250px", height:"33px"}} />
-              </Form>
-              <Nav className="ml-auto  ">
-                <Nav.Link className='navIcon' href="#home"  ><AiOutlineHome  style={{fontSize:"20px"}} /><div style={{fontSize:"13px"}}>Home</div></Nav.Link>
-                <Nav.Link className='navIcon' href="#network"  > <AiOutlineTeam style={{fontSize:"20px"}}/><div style={{fontSize:"13px"}}>My Network</div></Nav.Link>
-                <Nav.Link className='navIcon' href="#jobs"  ><RiBriefcaseLine  style={{fontSize:"20px"}}/><div style={{fontSize:"13px"}}> Jobs</div></Nav.Link>
-                <Nav.Link className='navIcon' href="#messaging" ><MdMessage style={{fontSize:"20px"}}/><div style={{fontSize:"13px"}}> Messaging</div></Nav.Link>
-                <Nav.Link className='navIcon' href="#notifications"  ><IoMdNotificationsOutline style={{fontSize:"20px"}}/><div style={{fontSize:"13px"}}> Notifications</div></Nav.Link>
-                <Nav.Link className='navIcon' href="#profile"  ><img src={photo} style={{borderRadius: "50%", height:"20px" , width:"20px" }}></img><div style={{fontSize:"13px"}} onClick="clickme()" >Me <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16" fill="currentColor" width="16" height="16" focusable="false">
+  render() {
+    return (
+      <Navbar
+        className="navbar mt-0 fixed-top "
+        style={{ height: "55px" }}
+        variant="dark"
+      >
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Linkedin_icon.svg/1200px-Linkedin_icon.svg.png"
+              className="linked"
+            ></img>{" "}
+          </Navbar.Brand>
+          <Form>
+            <Dropdown>
+            <FormControl
+              type="text"
+              placeholder="Search"
+              className="mr-sm-2"
+              style={{ width: "250px", height: "33px" }}
+              onChange={this.props.onsearch}
+            />
+            <Dropdown.Menu show={this.props.show}>
+             {this.props.users}
+            </Dropdown.Menu>
+            </Dropdown>
+          </Form>
+          <Nav className="ml-auto  ">
+            <Nav.Link className="navIcon" to="/Home">
+              <AiOutlineHome style={{ fontSize: "20px" }} />
+              <div style={{ fontSize: "13px" }}>Home</div>
+              
+            </Nav.Link>
+            <Nav.Link className="navIcon" >
+              {" "}
+              <AiOutlineTeam style={{ fontSize: "20px" }} />
+              <div style={{ fontSize: "13px" }}>My Network</div>
+            </Nav.Link>
+            <Nav.Link className="navIcon" >
+              <RiBriefcaseLine style={{ fontSize: "20px" }} />
+              <div style={{ fontSize: "13px" }}> Jobs</div>
+            </Nav.Link>
+            <Nav.Link className="navIcon" >
+              <MdMessage style={{ fontSize: "20px" }} />
+              <div style={{ fontSize: "13px" }}> Messaging</div>
+            </Nav.Link>
+            <Nav.Link className="navIcon" >
+              <IoMdNotificationsOutline style={{ fontSize: "20px" }} />
+              <div style={{ fontSize: "13px" }}> Notifications</div>
+            </Nav.Link>
+            <Nav.Link className="navIcon" >
+              <Link to="/home">
+              <img
+                src={photo}
+                style={{ borderRadius: "50%", height: "20px", width: "20px" }}
+              ></img>
+              <div style={{ fontSize: "13px" }}>
+                Me{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  data-supported-dps="16x16"
+                  fill="currentColor"
+                  width="16"
+                  height="16"
+                  focusable="false"
+                >
                   <path d="M8.8 10.66L14 5.12a.07.07 0 00-.07-.12H2.07a.07.07 0 00-.07.12l5.2 5.54a1.1 1.1 0 001.6 0z"></path>
-                  </svg></div></Nav.Link>
-                <Nav.Link style={{borderRight:"1px grey solid", height:"59px"}}></Nav.Link>
-                <Nav.Link className='navIcon' href="#work" ><BsGrid3X3GapFill style={{fontSize:"20px"}}/><div style={{fontSize:"13px"}}> Work <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16" fill="currentColor" width="16" height="16" focusable="false">
-                  <path d="M8.8 10.66L14 5.12a.07.07 0 00-.07-.12H2.07a.07.07 0 00-.07.12l5.2 5.54a1.1 1.1 0 001.6 0z"></path></svg></div></Nav.Link>
-                  <Nav.Link className='navIcon' href="#learning" ><AiOutlinePlaySquare style={{fontSize:"20px"}}/><div style={{fontSize:"13px"}}>Learning</div></Nav.Link>
-              </Nav>
-          </Container>
-        </Navbar>
-        )
-    }
+                </svg>
+              </div>
+              </Link>
+            </Nav.Link>
+            <Nav.Link
+              style={{ borderRight: "1px grey solid", height: "59px" }}
+            ></Nav.Link>
+            <Nav.Link className="navIcon" href="#work">
+              <BsGrid3X3GapFill style={{ fontSize: "20px" }} />
+              <div style={{ fontSize: "13px" }}>
+                {" "}
+                Work{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  data-supported-dps="16x16"
+                  fill="currentColor"
+                  width="16"
+                  height="16"
+                  focusable="false"
+                >
+                  <path d="M8.8 10.66L14 5.12a.07.07 0 00-.07-.12H2.07a.07.07 0 00-.07.12l5.2 5.54a1.1 1.1 0 001.6 0z"></path>
+                </svg>
+              </div>
+            </Nav.Link>
+            <Nav.Link className="navIcon" href="#learning">
+              <AiOutlinePlaySquare style={{ fontSize: "20px" }} />
+              <div style={{ fontSize: "13px" }}>Learning</div>
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    );
+  }
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Footer from "./Footer";
 import MainJumbotron from "./MainJumbotron";
 import SideBar from "./SideBar";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col,Dropdown } from "react-bootstrap";
 import NavBar from "./NavBar";
 import Experiences from "./Experiences";
 import axios from "axios";
@@ -10,27 +10,59 @@ import axios from "axios";
 export default class Profile extends Component {
   state = {
     username: this.props.match.params.id,
+    users :[],
     experiences: [],
+    show :false,
+    searchKey:'',
   };
   componentDidMount() {
     this.fetchExperience();
   }
+  
   async fetchExperience() {
-    let gets = {
+    let experience = {
       method: "GET",
       url: `https://striveschool.herokuapp.com/api/profile/${this.state.username}/experiences`,
       headers: {
         Authorization: "Basic " + btoa("user7:3UU5dYFvenRuRP7E"),
       },
     };
-    let experiences = await axios(gets);
-    this.setState({ experiences: experiences.data });
-    console.log(experiences);
+    let users = {
+      method: "GET",
+      url: `https://striveschool.herokuapp.com/api/profile/`,
+      headers: {
+        Authorization: "Basic " + btoa("user7:3UU5dYFvenRuRP7E"),
+      },
+    };
+    let experiences = await axios(experience);
+    let usersData = await axios(users)
+    this.setState({ experiences: experiences.data, users:usersData.data });
+    console.log(experiences,usersData.data);
   }
   render() {
     return (
       <>
-        <NavBar />
+        <NavBar 
+          onsearch={(e) => {
+            this.setState({searchKey:e.target.value.toLowerCase()})
+            console.log(e.target.value)
+            if(e.target.value.length >2) {
+              this.setState({show:true})
+            }else {this.setState({show:false})}}
+          }
+          show ={this.state.show}
+          users ={this.state.users.map((element,index) => {
+
+              if(element.name.toLowerCase().includes(this.state.searchKey)){
+                console.log(element.name)
+              return (
+                <Dropdown.Item 
+                key={index} 
+                eventKey={index}>{element.name}</Dropdown.Item>
+              )}
+
+          })}
+        />
         <Container style={{ marginTop: "58px" }}>
           <Row>
             <Col xs={8}>
@@ -41,7 +73,7 @@ export default class Profile extends Component {
                     image='https://www.careeraddict.com/uploads/article/55295/work-experience-note-pinboard.jpg'
                     role={element.role}
                     company={element.company}
-                    startDate={element.startDate}
+                    startDate={element.startDate.slice(0,10)}
                   />
                 );
               })}
