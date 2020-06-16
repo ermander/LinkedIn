9c96cbd39a1c2fbdcbe45b6bc25ce113dd821b2b
LaLe1993 <63840101+LaLe1993@users.noreply.github.com>
2020-06-16 16:15:51 +0200

    Update
    Added Path for myNetwork and Add Experience Form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Profile from './components/Profile'
 import Signin from './components/SignIn'
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import MyNetwork from './components/MyNetwork'; 
+import ExperienceSubmitionForm from './components/ExperienceSubmitionForm'
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <Switch>
         <Route exact path='/' component={Signin}/>
         <Route path='/profile/:id' component={Profile}/>
+        <Route path='/myNetwork' component={MyNetwork}/>
+        <Route exact path='/addExperience' component={ExperienceSubmitionForm} />
       </Switch>
     </Router>
      </>
