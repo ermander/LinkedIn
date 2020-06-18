13d1e958463ac8a70b24928bb048e1865ad8fb9e
Detart <63840243+gdetart@users.noreply.github.com>
2020-06-18 12:28:46 +0200

    upload pics

--- src/components/Homepage/Feed.jsx
+++ src/components/Homepage/Feed.jsx
@@ -1,78 +1,103 @@
-import React, { Component } from 'react'
-import {Container,Modal,Button} from 'react-bootstrap'
-import {FiEdit,FiVideo} from 'react-icons/fi'
-import {GrCamera} from 'react-icons/gr'
-import {IconContext} from 'react-icons'
-import {AiOutlineFileText} from 'react-icons/ai'
-import Posts from './Posts'
+import React, { Component } from "react";
+import { Container, Modal, Button } from "react-bootstrap";
+import { FiEdit, FiVideo } from "react-icons/fi";
+import { GrCamera } from "react-icons/gr";
+import { IconContext } from "react-icons";
+import { AiOutlineFileText } from "react-icons/ai";
+import Posts from "./Posts";
 // import UploadPic from '../UploadPic'
 
 export class Feed extends Component {
-    state ={
-        posts :[],
-        username:[]
-    }
-    componentDidMount = async()=>{
-        let response = await fetch('https://striveschool.herokuapp.com/api/posts/',{
-            method :'GET',
-            headers : new Headers({
-                'Authorization': "Basic " + btoa("user7:3UU5dYFvenRuRP7E"),
-                'Content-type': "application/json"
-              })
-        })
-        let user = await fetch('https://striveschool.herokuapp.com/api/profile/me',{
-            method :'GET',
-            headers : new Headers({
-               'Authorization': "Basic " + btoa("user7:3UU5dYFvenRuRP7E"),
-                'Content-type': "application/json"
-              })
-        })
-        let userName = await user.json()
-        let posts = await response.json()
-        this.setState({posts,username:userName},()=>console.log(this.state.username))
-        console.log(this.state.posts)
-    }
-    handleShow =()=>{
-        this.setState({show:true})
-    }
-    handleClose =()=>{
-        this.setState({show:false})
-    }
-    
-    render() {
-        return (
-            <Container fluid  >
-                <div id='writePost' >
-                    <div onClick={this.props.postButton}>
-                        <IconContext.Provider value={{className:'writePostIcons'}}>
-                            <a><FiEdit/></a>
-                        </IconContext.Provider>
-                        <a style={{fontSize:'20px',paddingTop:'20px'}}>Start a Post</a>
-                    </div>
-                    <div id='icons'>
-                        <IconContext.Provider value={{className : "writePostIcons"}}>
-                            <div><GrCamera/></div>
-                        </IconContext.Provider>
-                        <IconContext.Provider value={{className : "writePostIcons"}}>
-                            <div><FiVideo/></div>
-                        </IconContext.Provider>
-                        <IconContext.Provider value={{className : "writePostIcons"}}>
-                            <div><AiOutlineFileText/></div>
-                        </IconContext.Provider>
-                    </div>
-                </div>
-                <div id='writePostFooter'>
-                    <p><a href="">Write an article</a> on LinkedIn</p>
-                </div>
-                <hr></hr>
-                {this.state.posts.map(element =>{
-                    return(
-                        <Posts user={this.state.username.name} name={element.user.name} bio={element.user.bio} text={element.text}/>
-                    )
-                })}
-            </Container>
-        )
-    }
+  state = {
+    posts: [],
+    username: [],
+  };
+  componentDidMount = async () => {
+    let response = await fetch(
+      "https://striveschool.herokuapp.com/api/posts/",
+      {
+        method: "GET",
+        headers: new Headers({
+          Authorization: "Basic " + btoa("user7:3UU5dYFvenRuRP7E"),
+          "Content-type": "application/json",
+        }),
+      }
+    );
+    let user = await fetch(
+      "https://striveschool.herokuapp.com/api/profile/me",
+      {
+        method: "GET",
+        headers: new Headers({
+          Authorization: "Basic " + btoa("user7:3UU5dYFvenRuRP7E"),
+          "Content-type": "application/json",
+        }),
+      }
+    );
+    let userName = await user.json();
+    let posts = await response.json();
+    this.setState({ posts, username: userName }, () =>
+      console.log(this.state.username)
+    );
+    console.log(this.state.posts);
+  };
+  handleShow = () => {
+    this.setState({ show: true });
+  };
+  handleClose = () => {
+    this.setState({ show: false });
+  };
+
+  render() {
+    return (
+      <Container fluid>
+        <div id="writePost">
+          <div onClick={this.props.postButton}>
+            <IconContext.Provider value={{ className: "writePostIcons" }}>
+              <a>
+                <FiEdit />
+              </a>
+            </IconContext.Provider>
+            <a style={{ fontSize: "20px", paddingTop: "20px" }}>Start a Post</a>
+          </div>
+          <div id="icons">
+            <IconContext.Provider value={{ className: "writePostIcons" }}>
+              <div>
+                <GrCamera />
+              </div>
+            </IconContext.Provider>
+            <IconContext.Provider value={{ className: "writePostIcons" }}>
+              <div>
+                <FiVideo />
+              </div>
+            </IconContext.Provider>
+            <IconContext.Provider value={{ className: "writePostIcons" }}>
+              <div>
+                <AiOutlineFileText />
+              </div>
+            </IconContext.Provider>
+          </div>
+        </div>
+        <div id="writePostFooter">
+          <p>
+            <a href="">Write an article</a> on LinkedIn
+          </p>
+        </div>
+        <hr></hr>
+        {this.state.posts.map((element) => {
+          return (
+            <Posts
+              user={this.state.username.name}
+              name={element.user.name}
+              id={element._id}
+              image={element.image}
+              bio={element.user.bio}
+              text={element.text}
+            />
+          );
+        })}
+      </Container>
+    );
+  }
 }
 
-export default Feed
+export default Feed;
