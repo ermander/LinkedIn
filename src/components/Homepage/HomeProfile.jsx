5de4bd99b1e9ea7346ab879afdb82ec2776fec00
LaLe1993 <63840101+LaLe1993@users.noreply.github.com>
2020-06-18 16:35:17 +0200

    Removed borders

--- src/components/Homepage/HomeProfile.jsx
+++ src/components/Homepage/HomeProfile.jsx
@@ -21,13 +21,13 @@ render(){
                 className="img-fluid rounded-circle first"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1200px-Circle-icons-profile.svg.png"
               />
-              <div className=" home p-2 text-center">
+              <div className=" home p-2 text-center" style={{borderBottom: '0px'}}>
                 <p className="p1">
                   Welcome, {this.props.name}
                 </p>
                 <a  className="a1">Update your profile</a>
               </div>
-              <div className="p-2  home">
+              <div className="p-2  home"  style={{borderBottom: '0px'}}>
                 <p className="p2" >
                   Who viewed your profile{" "}
                 </p>
@@ -36,7 +36,7 @@ render(){
                   Grow your Network
                 </p>
               </div>
-              <div className="p-2  home">
+              <div className="p-2  home"  style={{borderBottom: '0px'}}>
                 <p className="p3" >
                   See all premium features
                 </p>
